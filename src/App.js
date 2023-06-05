80b976a227dbe1ce3255cc215ce03efe3106e7c6
Sourav Tiwari <souravtiwari@Souravs-MacBook-Pro.local>
2023-06-06 04:47:30 +0530

    product listing created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import ProductListing from "./ProductListing";
 import "./App.css";
 
 function App() {
-  return <div className="App">Product Feature</div>;
+  return (
+    <div className="App">
+      <div>Product Listing</div>
+      <ProductListing />
+    </div>
+  );
 }
 
 export default App;
