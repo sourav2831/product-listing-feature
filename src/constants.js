5b45367b69fdf580c7fb2c501928cbd73466cfb1
Sourav Tiwari <souravtiwari@Souravs-MacBook-Pro.local>
2023-06-06 04:46:45 +0530

    added constants

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const PRODUCT_API = "https://fakestoreapi.com/products/";
+
+export const RATING = ["1-2", "2-3", "3-4", "4+"];
+
+export const PRICE = ["<10", "10-50", "50-100", "100-500", "500+"];
